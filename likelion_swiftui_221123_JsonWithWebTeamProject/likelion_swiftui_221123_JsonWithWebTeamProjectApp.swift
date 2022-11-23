5ee87cdc8208459655da2b90395b197b675238da
Taeyoung Won <hexter1994@gmail.com>
2022-11-23 10:37:00 +0900

    Initial Commit

---
+++ likelion_swiftui_221123_JsonWithWebTeamProject/likelion_swiftui_221123_JsonWithWebTeamProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  likelion_swiftui_221123_JsonWithWebTeamProjectApp.swift
+//  likelion_swiftui_221123_JsonWithWebTeamProject
+//
+//  Created by 원태영 on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct likelion_swiftui_221123_JsonWithWebTeamProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
